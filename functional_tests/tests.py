e400120c844b7b1f459dc2469f9b441de0cf8772
Niroshan Devendran <ndevendran@Niroshans-MacBook-Pro.local>
2019-06-05 15:28:46 -0700

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -38,6 +38,11 @@ class NewVisitorTest(LiveServerTestCase):
         # Edith has heard about a cool new online to-do app.
         # She goes to check out its homepage
         self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width']/2, 512, delta=10)
 
 
         # She notices the page title and header mention to-do lists
@@ -116,6 +121,20 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
+
+    def test_element_alignment_on_list_app(self):
+        
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+
+        #User starts a new list and sees the input
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width']/2, 512, delta=10)
+
 
 
 
