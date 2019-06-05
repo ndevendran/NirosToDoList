45de9102752c47141d7122b79584c3d76168dc66
Niroshan Devendran <ndevendran@Niroshans-MacBook-Pro.local>
2019-06-04 18:36:38 -0700

    Added FT to test for unique lists

--- lists/tests.py
+++ lists/tests.py
@@ -24,7 +24,7 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], 'lists/the-only-list-in-the-world/')
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
@@ -35,6 +35,8 @@ class HomePageTest(TestCase):
         Item.objects.create(text='itemey 2')
 
         response = self.client.get('/')
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
