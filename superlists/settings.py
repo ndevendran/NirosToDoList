a6297145d3f4731fe7078fb0723b116fad279121
Niroshan Devendran <ndevendran@Niroshans-MacBook-Pro.local>
2019-06-05 20:20:42 -0700

    Updated django project settings and Procfile for deployment

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'h-%g3x8mst01(+#jm&d^il!2u2f6mjtrdi$d@4n#bk9i=8b5)e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
