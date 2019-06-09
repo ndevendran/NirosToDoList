3d37977ba4250baa571f79e4dccffc5b092f90a6
Niroshan Devendran <ndevendran@Niroshans-MacBook-Pro.local>
2019-06-09 00:03:06 -0700

    Added model validation for blank list items

--- lists/models.py
+++ lists/models.py
@@ -6,7 +6,7 @@ class List(models.Model):
 
 class Item(models.Model):
     text = models.TextField(default='')
-    list = models.ForeignKey(List, default=None, on_delete=models.PROTECT)
+    list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
 
 
 
